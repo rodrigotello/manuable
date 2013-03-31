0b2d2e487ffa03fb9e855523f50514c7836eb483
goreorto <cesar.cejudo@gmail.com>
2013-03-30 19:20:22 -0600

    config carrier wave aws

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,9 +2,7 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],                        # required
-    :aws_secret_access_key  => ENV['AWS_SECRET_KEY'],                        # required
-    :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
-    :endpoint               => 'manuable.s3-website-us-east-1.amazonaws.com' # optional, defaults to nil
+    :aws_secret_access_key  => ENV['AWS_SECRET_KEY']
   }
   config.fog_directory  = 'manuable'                     # required
   config.fog_public     = false                                   # optional, defaults to true
