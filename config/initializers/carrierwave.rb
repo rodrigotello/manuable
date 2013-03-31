bd734f7d0a6e4ff9a524107441d5d205d815888f
goreorto <cesar.cejudo@gmail.com>
2013-03-30 19:16:32 -0600

    bucket configuration

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,8 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],                        # required
     :aws_secret_access_key  => ENV['AWS_SECRET_KEY'],                        # required
-    :region                 => 'eu-west-1'#,                  # optional, defaults to 'us-east-1'
-    #:host                   => 's3.example.com',             # optional, defaults to nil
-    #:endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
+    :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
+    :endpoint               => 'manuable.s3-website-us-east-1.amazonaws.com' # optional, defaults to nil
   }
   config.fog_directory  = 'manuable'                     # required
   config.fog_public     = false                                   # optional, defaults to true
