d61decb438e70325bf4a35194b26310b3b73eace
goreorto <cesar.cejudo@gmail.com>
2013-05-03 00:33:21 -0500

    profile edit

--- app/controllers/my/profiles_controller.rb
+++ app/controllers/my/profiles_controller.rb
@@ -1,5 +1,7 @@
 module My
   class ProfilesController < ApplicationController
+    before_filter :authenticate_user!
+
     layout 'my'
 
     def show
@@ -7,8 +9,21 @@ module My
     end
 
     def edit
-      @my_section = "settings"
+      @my_section = params[:f] == 's' ? "name" : 'password'
     end
 
+    def update
+      @user = current_user
+      if params[:password].present? && params[:password_confirmation].present? && params[:current_password].present?
+        current_user.update_with_password params[:user]
+      else
+        params[:user].delete :password
+        params[:user].delete :password_confirmation
+        params[:user].delete :current_password
+        current_user.update_attributes params[:user]
+      end
+      sign_in @user, :bypass => true
+      redirect_to :back
+    end
   end
 end
