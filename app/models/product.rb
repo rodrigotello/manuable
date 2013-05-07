5a33d45b8d048b059940fb75d7be28531768858e
goreorto <cesar.cejudo@gmail.com>
2013-05-06 22:13:02 -0500

    languagues, bug fixes

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,11 +8,12 @@ class Product < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
   has_many :attachments, as: :attachable, dependent: :destroy
-  has_many :likes
+  has_many :likes, dependent: :destroy
   has_many :likers, through: :like, source: :user
 
   accepts_nested_attributes_for :attachments, reject_if: proc{ |at| at[:attachment].blank? }, limit: 4
   # validates :name, presence: true
+  before_destroy :delete_activities
 
   def ready?
     name.present? && attachments.count > 0 && category.present?
@@ -20,5 +21,11 @@ class Product < ActiveRecord::Base
 
   def like! current_user
     likes.where(user_id: current_user.id).first_or_create
+  end
+
+  private
+
+  def delete_activities
+    activities.destroy_all
   end
 end
