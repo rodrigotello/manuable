a8a24b459ae2c87d0d112d7b82c49ce31557a085
goreorto <cesar.cejudo@gmail.com>
2013-05-13 23:23:17 -0500

    seguidores

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -15,7 +15,7 @@ var UsersShow = new function(){
       $this.replaceWith('<span class="btn btn-mini active"><i class="icon-ok"></i> Siguiendo</span>');
 
       var li = "<li class='media'>";
-      li += '<img class="media-object" src="'+ current_user.avatar.small.url +'" alt="'+current_user.name+'"/>';
+      li += '<img class="media-object pull-left" src="'+ current_user.avatar.small.url +'" alt="'+current_user.name+'"/>';
       li += '<div class="media-body">';
       li += ('<a href="/users/"'+current_user.id+'">' + current_user.name + '</a>');
       li += '</div>';
