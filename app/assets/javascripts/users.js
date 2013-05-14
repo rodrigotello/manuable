b2006e2ef99eddb1d2ba14a8321b442eaee16de6
goreorto <cesar.cejudo@gmail.com>
2013-05-13 23:20:56 -0500

    seguidores

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -13,7 +13,7 @@ var UsersShow = new function(){
     $('#follow').bind('ajax:success', function(){
       var $this = $(this);
       $this.replaceWith('<span class="btn btn-mini active"><i class="icon-ok"></i> Siguiendo</span>');
-                = link_to u.name, u
+
       var li = "<li class='media'>";
       li += '<img class="media-object" src="'+ current_user.avatar.small.url +'" alt="'+current_user.name+'"/>';
       li += '<div class="media-body">';
