b3fc637a81867d74deeaaf9fd1aa7e9ab7ac3850
goreorto <cesar.cejudo@gmail.com>
2013-07-21 20:59:57 -0500

    conversations

--- config/routes.rb
+++ config/routes.rb
@@ -99,6 +99,6 @@ Manuable::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-   match '/(:id)', controller: :users, action: :show
+  # match '/(:id)', controller: :users, action: :show
 
 end
