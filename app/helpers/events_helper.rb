297dad34085f377801d65bc3fe773461269bd3f8
goreorto <cesar.cejudo@gmail.com>
2013-10-20 09:46:37 -0500

    popup request access

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,15 +1,10 @@
 #encoding: utf-8
 module EventsHelper
-  def request_link event
+  def request_link event, text=nil
     output = ""
     if signed_in? && (event_request = event.event_requests.where(user_id: current_user.id).first)
       if event_request.accepted.nil?
-        output << "<div class='disabled request-access'>
-
-          Esperando
-
-          respuesta
-        </div>"
+        output << "<div class='disabled request-access'>Esperando respuesta</div>"
       elsif event_request.accepted
         output << link_to("¡Aceptada! pasar a paga".html_safe, checkout_event_path(event), class: 'request-access')
       else
@@ -20,9 +15,9 @@ module EventsHelper
     else
       if event.seats_left?
         if signed_in?
-          output << link_to('¡Quiero participar!'.html_safe, request_access_event_path, class: 'request-access', rel: 'modal', title: '¡Quiero participar!', data: { modalclass: 'request-access-modal' })
+          output << link_to("¡#{ text || "Quiero participar" }!".html_safe, request_access_event_path, class: 'request-access', rel: 'modal', title: '¡Quiero participar!', data: { modalclass: 'request-access-modal' })
         else
-          output << link_to('¡Quiero participar!'.html_safe, "#sign_popup", "data-toggle" => "modal", :role => "button", class: 'request-access')
+          output << link_to("¡#{ text || "Quiero participar" }!".html_safe, "#sign_popup", "data-toggle" => "modal", :role => "button", class: 'request-access')
         end
       end
     end
