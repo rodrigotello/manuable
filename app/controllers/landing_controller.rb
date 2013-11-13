5d651c5b4a2d7a5647a4592fd0f1d1fae934f121
goreorto <cesar.cejudo@gmail.com>
2013-11-12 21:00:58 -0600

    fix

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -3,7 +3,7 @@ class LandingController < ApplicationController
     user = User.where(:nickname => params[:id]).first
     redirect_to user and return if user.present?
 
-    user = Event.incoming.where(:slug => params[:id]).first
+    event = Event.incoming.where(:slug => params[:id]).first
     redirect_to event and return if user.present?
 
     redirect_to root_path
