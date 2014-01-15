465ee932ca10c2870e2c4d99d051171398cc0ee9
César Israel Cejudo Fontes <goreorto@Cesars-MacBook-Pro.local>
2014-01-14 22:19:03 -0600

    public uploads

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => ENV['AWS_SECRET_KEY']
   }
   config.fog_directory  = 'manuable'                     # required
-  config.fog_public     = false                                   # optional, defaults to true
+  config.fog_public     = true                                   # optional, defaults to true
   config.fog_attributes = { 'Cache-Control' => 'max-age=315576000' }  # optional, defaults to {}
   config.asset_host       = "//manuable.s3.amazonaws.com/"
 
