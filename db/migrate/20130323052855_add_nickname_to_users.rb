6e5b6ce1ecf711b4ee238ca11ecfe89b861e63b7
Goreorto <cesar@letwrong.com>
2014-05-17 08:34:47 -0500

    fix migration

--- db/migrate/20130323052855_add_nickname_to_users.rb
+++ db/migrate/20130323052855_add_nickname_to_users.rb
@@ -1,6 +1,6 @@
 class AddNicknameToUsers < ActiveRecord::Migration
   def change
     add_column :users, :nickname, :string
-    add_index :users, :nickname, uniq: true
+    add_index :users, :nickname, unique: true
   end
 end
