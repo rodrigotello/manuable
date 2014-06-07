55fe63d14602fdadea6eefd4b5a152c05dda7317
Goreorto <cesar@letwrong.com>
2014-06-07 13:16:37 -0500

    new bucket

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,8 @@ CarrierWave.configure do |config|
     :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],                        # required
     :aws_secret_access_key  => ENV['AWS_SECRET_KEY']
   }
-  config.fog_directory  = 'manuable'                     # required
+  config.cache_dir = "#{Rails.root}/tmp/uploads"
+  config.fog_directory  = 'manuable02'                     # required
   config.fog_public     = true                                   # optional, defaults to true
   config.fog_attributes = { 'Cache-Control' => 'max-age=315576000' }  # optional, defaults to {}
   # config.asset_host       = "//manuable.s3.amazonaws.com/"
